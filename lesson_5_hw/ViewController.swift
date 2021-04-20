70396598461a804d6bcbf44df454abacc2d9b8a2
Юрий Егоров <y.egorov@city-mobil.ru>
2021-04-20 21:10:54 +0300

    Initial Commit

---
+++ lesson_5_hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_5_hw
+//
+//  Created by Юрий Егоров on 20.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
